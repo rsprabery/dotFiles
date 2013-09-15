6554f130eab8f21b3ff689bc11fdc2c809c0d5b5
Read Sprabery <rsprabery@users.noreply.github.com>
2013-09-15 15:41:13 -0500

    Added oh-my-zsh installation, changes the shell to zsh, sets parameters for git (name, email, editor)

--- setup.sh
+++ setup.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+
+# install oh-my-zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+# Change default shell
+chsh -s /bin/zsh
+
+# Links configs
 dir=`pwd`
 files="zshrc vimrc tmux.conf gemrc"
 for file in $files; do
   ln -s $dir/$file ~/.$file
 done
+
+# install vundle for vim
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+# setup git
+git config --global user.email "read.sprabery@gmail.com"
+git config --global user.name "Read Sprabery"
+git config --global core.editor vim
