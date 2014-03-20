7ca11f9c878077dba5b190f146f9ed202ffbc5b7
Read Sprabery <rsprabery@users.noreply.github.com>
2014-03-19 21:36:03 -0500

    Removed an unnecessary script

--- install_scripts/scriptSetup.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-mkdir -p ~/software
-cd ~/software
-git clone git@dev.sprabery.com:shell_scripts
-
-ln -s ~/software/shell_scripts/lsprojs.sh ~/bin/lsprojs
-ln -s ~/software/shell_scripts/mkproj.sh ~/bin/mkproj
-
