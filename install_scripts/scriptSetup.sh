25b485fd9f4cf72c9bb36a21a6b26c334e146baf
Read Sprabery <rsprabery@users.noreply.github.com>
2014-03-19 21:05:38 -0500

    Changed location of git repo

--- install_scripts/scriptSetup.sh
+++ install_scripts/scriptSetup.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/software
 cd ~/software
-git clone git@sprabery.com:shell_scripts
+git clone git@dev.sprabery.com:shell_scripts
 
 ln -s ~/software/shell_scripts/lsprojs.sh ~/bin/lsprojs
 ln -s ~/software/shell_scripts/mkproj.sh ~/bin/mkproj
