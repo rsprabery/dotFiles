63b1b29a952392e4e984ad80c8a6ec7346e49a5d
Read Sprabery <rsprabery@users.noreply.github.com>
2016-12-15 08:54:05 -0600

    Fixed missing deps

--- install_scripts/ubuntu/base.sh
+++ install_scripts/ubuntu/base.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sudo apt-get --assume-yes install git curl wget vim tmux zsh build-essential \
-  gcc dkms colordiff cmake ex python-dev python3-dev exuberant-ctags clang-3.8
+  gcc dkms colordiff cmake python-dev python3-dev exuberant-ctags clang-3.8 \
+  ruby-dev
